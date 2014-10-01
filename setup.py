810f99d804f8c05ef95517baf1a374c414f155df
HashBang <hashbangstudio@users.noreply.github.com>
2014-10-01 22:25:24 +0100

    Update setup.py to inline with master
    This should hopefully mean that build will pass with enum dependency met

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 import io
 from setuptools import setup, find_packages
+import sys
 
 with io.open('README.rst', mode='r', encoding='utf8') as f:
     readme = f.read()
+
+
+dependencies = []
+if sys.version_info[:2] < (3, 4):
+    dependencies.append('enum34')
+
 
 setup(name='py3minepi',
       version='0.0.1',
@@ -14,17 +21,18 @@ setup(name='py3minepi',
       include_package_data=True,
       keywords='minecraft raspberry pi mcpi py3minepi',
       long_description=readme,
+      install_requires=dependencies,
       classifiers=[
-          'Development Status :: Development Status :: 3 - Alpha',
-          'Environment :: X11 Applications',
-          'Intended Audience :: Education',
-          'Intended Audience :: Developers',
-          'License :: OSI Approved :: Other/Proprietary License',  # TODO fix
-          'Operating System :: POSIX',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-      ],
-)
+                   'Development Status :: Development Status :: 3 - Alpha',
+                   'Environment :: X11 Applications',
+                   'Intended Audience :: Education',
+                   'Intended Audience :: Developers',
+                   'License :: OSI Approved :: Other/Proprietary License',  # TODO fix
+                   'Operating System :: POSIX',
+                   'Operating System :: POSIX :: Linux',
+                   'Programming Language :: Python :: 3',
+                   'Programming Language :: Python :: 3.2',
+                   'Programming Language :: Python :: 3.3',
+                   'Programming Language :: Python :: 3.4',
+                   ],
+      )
