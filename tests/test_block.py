a3e3a0038f11d42c411ffcef7616a458d69f4b0a
HashBang <hashbangstudio@users.noreply.github.com>
2014-10-19 06:33:28 +0100

    Fix conflict between files
    both added test_block.py.
    
    Combined in one file

--- tests/test_block.py
+++ tests/test_block.py
@@ -1,6 +1,12 @@
-<<<<<<< HEAD
-from minecraft.block import Block
-from minecraft.block import BlockType
+"""
+Tests for the Block, BlockType classes and named Block instances
+"""
+from mcpi import block
+from mcpi.block import Block
+from mcpi.block import BlockType
+
+
+block.Block(block.AIR)
 
 
 class TestBlock():
@@ -125,12 +131,147 @@ class TestBlock():
         assert BlockType.FENCE_GATE == 107
         assert BlockType.GLOWING_OBSIDIAN == 246
         assert BlockType.NETHER_REACTOR_CORE == 247
-=======
-"""
-Bare-bones but we don't even import block in any tests yet
-"""
 
-from mcpi import block
+    def test_block_instances(self):
+        assert block.AIR == Block(0, 0)
+        assert block.STONE == Block(1, 0)
+        assert block.GRASS == Block(2, 0)
+        assert block.DIRT == Block(3, 0)
+        assert block.COBBLESTONE == Block(4, 0)
+        assert block.WOOD_PLANKS == Block(5, 0)
+        assert block.SAPLING == Block(6, 0)
+        assert block.BEDROCK == Block(7, 0)
+        assert block.WATER_FLOWING == Block(8, 0)
+        assert block.WATER_STATIONARY == Block(9, 0)
+        assert block.LAVA_FLOWING == Block(10, 0)
+        assert block.LAVA_STATIONARY == Block(11, 0)
+        assert block.SAND == Block(12, 0)
+        assert block.GRAVEL == Block(13, 0)
+        assert block.GOLD_ORE == Block(14, 0)
+        assert block.IRON_ORE == Block(15, 0)
+        assert block.COAL_ORE == Block(16, 0)
+        assert block.WOOD == Block(17, 0)
+        assert block.LEAVES == Block(18, 0)
+        assert block.GLASS == Block(20, 0)
+        assert block.LAPIS_LAZULI_ORE == Block(21, 0)
+        assert block.LAPIS_LAZULI_BLOCK == Block(22, 0)
+        assert block.SANDSTONE == Block(24, 0)
+        assert block.BED == Block(26, 0)
+        assert block.COBWEB == Block(30, 0)
+        assert block.GRASS_TALL == Block(31, 0)
+        assert block.WOOL == Block(35, 0)
+        assert block.FLOWER_YELLOW == Block(37, 0)
+        assert block.FLOWER_CYAN == Block(38, 0)
+        assert block.MUSHROOM_BROWN == Block(39, 0)
+        assert block.MUSHROOM_RED == Block(40, 0)
+        assert block.GOLD_BLOCK == Block(41, 0)
+        assert block.IRON_BLOCK == Block(42, 0)
+        assert block.STONE_SLAB_DOUBLE == Block(43, 0)
+        assert block.STONE_SLAB == Block(44, 0)
+        assert block.BRICK_BLOCK == Block(45, 0)
+        assert block.TNT == Block(46, 0)
+        assert block.BOOKSHELF == Block(47, 0)
+        assert block.MOSS_STONE == Block(48, 0)
+        assert block.OBSIDIAN == Block(49, 0)
+        assert block.TORCH == Block(50, 0)
+        assert block.FIRE == Block(51, 0)
+        assert block.STAIRS_WOOD == Block(53, 0)
+        assert block.CHEST == Block(54, 0)
+        assert block.DIAMOND_ORE == Block(56, 0)
+        assert block.DIAMOND_BLOCK == Block(57, 0)
+        assert block.CRAFTING_TABLE == Block(58, 0)
+        assert block.FARMLAND == Block(60, 0)
+        assert block.FURNACE_INACTIVE == Block(61, 0)
+        assert block.FURNACE_ACTIVE == Block(62, 0)
+        assert block.DOOR_WOOD == Block(64, 0)
+        assert block.LADDER == Block(65, 0)
+        assert block.STAIRS_COBBLESTONE == Block(67, 0)
+        assert block.DOOR_IRON == Block(71, 0)
+        assert block.REDSTONE_ORE == Block(73, 0)
+        assert block.SNOW == Block(78, 0)
+        assert block.ICE == Block(79, 0)
+        assert block.SNOW_BLOCK == Block(80, 0)
+        assert block.CACTUS == Block(81, 0)
+        assert block.CLAY == Block(82, 0)
+        assert block.SUGAR_CANE == Block(83, 0)
+        assert block.FENCE == Block(85, 0)
+        assert block.GLOWSTONE_BLOCK == Block(89, 0)
+        assert block.BEDROCK_INVISIBLE == Block(95, 0)
+        assert block.STONE_BRICK == Block(98, 0)
+        assert block.GLASS_PANE == Block(102, 0)
+        assert block.MELON == Block(103, 0)
+        assert block.FENCE_GATE == Block(107, 0)
+        assert block.GLOWING_OBSIDIAN == Block(246, 0)
+        assert block.NETHER_REACTOR_CORE == Block(247, 0)
 
-block.Block(block.AIR)
->>>>>>> upstream/master
+    def test_block_constants_equal_instances(self):
+        assert BlockType.AIR == block.AIR.id
+        assert BlockType.STONE == block.STONE.id
+        assert BlockType.GRASS == block.GRASS.id
+        assert BlockType.DIRT == block.DIRT.id
+        assert BlockType.COBBLESTONE == block.COBBLESTONE.id
+        assert BlockType.WOOD_PLANKS == block.WOOD_PLANKS.id
+        assert BlockType.SAPLING == block.SAPLING.id
+        assert BlockType.BEDROCK == block.BEDROCK.id
+        assert BlockType.WATER_FLOWING == block.WATER_FLOWING.id
+        assert BlockType.WATER_STATIONARY == block.WATER_STATIONARY.id
+        assert BlockType.LAVA_FLOWING == block.LAVA_FLOWING.id
+        assert BlockType.LAVA_STATIONARY == block.LAVA_STATIONARY.id
+        assert BlockType.SAND == block.SAND.id
+        assert BlockType.GRAVEL == block.GRAVEL.id
+        assert BlockType.GOLD_ORE == block.GOLD_ORE.id
+        assert BlockType.IRON_ORE == block.IRON_ORE.id
+        assert BlockType.COAL_ORE == block.COAL_ORE.id
+        assert BlockType.WOOD == block.WOOD.id
+        assert BlockType.LEAVES == block.LEAVES.id
+        assert BlockType.GLASS == block.GLASS.id
+        assert BlockType.LAPIS_LAZULI_ORE == block.LAPIS_LAZULI_ORE.id
+        assert BlockType.LAPIS_LAZULI_BLOCK == block.LAPIS_LAZULI_BLOCK.id
+        assert BlockType.SANDSTONE == block.SANDSTONE.id
+        assert BlockType.BED == block.BED.id
+        assert BlockType.COBWEB == block.COBWEB.id
+        assert BlockType.GRASS_TALL == block.GRASS_TALL.id
+        assert BlockType.WOOL == block.WOOL.id
+        assert BlockType.FLOWER_YELLOW == block.FLOWER_YELLOW.id
+        assert BlockType.FLOWER_CYAN == block.FLOWER_CYAN.id
+        assert BlockType.MUSHROOM_BROWN == block.MUSHROOM_BROWN.id
+        assert BlockType.MUSHROOM_RED == block.MUSHROOM_RED.id
+        assert BlockType.GOLD_BLOCK == block.GOLD_BLOCK.id
+        assert BlockType.IRON_BLOCK == block.IRON_BLOCK.id
+        assert BlockType.STONE_SLAB_DOUBLE == block.STONE_SLAB_DOUBLE.id
+        assert BlockType.STONE_SLAB == block.STONE_SLAB.id
+        assert BlockType.BRICK_BLOCK == block.BRICK_BLOCK.id
+        assert BlockType.TNT == block.TNT.id
+        assert BlockType.BOOKSHELF == block.BOOKSHELF.id
+        assert BlockType.MOSS_STONE == block.MOSS_STONE.id
+        assert BlockType.OBSIDIAN == block.OBSIDIAN.id
+        assert BlockType.TORCH == block.TORCH.id
+        assert BlockType.FIRE == block.FIRE.id
+        assert BlockType.STAIRS_WOOD == block.STAIRS_WOOD.id
+        assert BlockType.CHEST == block.CHEST.id
+        assert BlockType.DIAMOND_ORE == block.DIAMOND_ORE.id
+        assert BlockType.DIAMOND_BLOCK == block.DIAMOND_BLOCK.id
+        assert BlockType.CRAFTING_TABLE == block.CRAFTING_TABLE.id
+        assert BlockType.FARMLAND == block.FARMLAND.id
+        assert BlockType.FURNACE_INACTIVE == block.FURNACE_INACTIVE.id
+        assert BlockType.FURNACE_ACTIVE == block.FURNACE_ACTIVE.id
+        assert BlockType.DOOR_WOOD == block.DOOR_WOOD.id
+        assert BlockType.LADDER == block.LADDER.id
+        assert BlockType.STAIRS_COBBLESTONE == block.STAIRS_COBBLESTONE.id
+        assert BlockType.DOOR_IRON == block.DOOR_IRON.id
+        assert BlockType.REDSTONE_ORE == block.REDSTONE_ORE.id
+        assert BlockType.SNOW == block.SNOW.id
+        assert BlockType.ICE == block.ICE.id
+        assert BlockType.SNOW_BLOCK == block.SNOW_BLOCK.id
+        assert BlockType.CACTUS == block.CACTUS.id
+        assert BlockType.CLAY == block.CLAY.id
+        assert BlockType.SUGAR_CANE == block.SUGAR_CANE.id
+        assert BlockType.FENCE == block.FENCE.id
+        assert BlockType.GLOWSTONE_BLOCK == block.GLOWSTONE_BLOCK.id
+        assert BlockType.BEDROCK_INVISIBLE == block.BEDROCK_INVISIBLE.id
+        assert BlockType.STONE_BRICK == block.STONE_BRICK.id
+        assert BlockType.GLASS_PANE == block.GLASS_PANE.id
+        assert BlockType.MELON == block.MELON.id
+        assert BlockType.FENCE_GATE == block.FENCE_GATE.id
+        assert BlockType.GLOWING_OBSIDIAN == block.GLOWING_OBSIDIAN.id
+        assert BlockType.NETHER_REACTOR_CORE == block.NETHER_REACTOR_CORE.id
